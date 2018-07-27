9f9d45317ac1e779f352b314f04e15055dcb5ce2
dyavani.narsingrao@gmail.com <Priya0*$>
2018-07-27 00:58:22 -0700

    Interactivity Demo
    Signed-off-by: dyavani.narsingrao@gmail.com

--- app/src/main/java/com/example/ndyavani/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/ndyavani/exampleapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ndyavani.exampleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view){
+        Log.i("info", "Butten Pressed");
     }
 }
